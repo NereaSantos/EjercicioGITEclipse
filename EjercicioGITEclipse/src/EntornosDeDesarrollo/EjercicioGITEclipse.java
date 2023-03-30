4cadcc2212fa092972a4f198916a22fbfd89c7b4
nerea <santoscanalnerea@gmail.com>
2023-03-30 09:30:08 +0200

    Instanciar la clase y mostrarla

--- EjercicioGITEclipse/src/EntornosDeDesarrollo/EjercicioGITEclipse.java
+++ EjercicioGITEclipse/src/EntornosDeDesarrollo/EjercicioGITEclipse.java
@@ -6,6 +6,10 @@ public class EjercicioGITEclipse {
 		
 		System.out.println("Lista de alumnos 1º DAW");
 		System.out.println("Alumno1");
+	
+		Alumno();
+		
+		System.out.println(Alumno.toString());
 		
 	}
 	
